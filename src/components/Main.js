24eab1f6a46910dfa1ae8aff92e4746204c7c642
Marina Abovyan <marrina.abovyan@gmail.com>
2021-06-05 16:35:08 +0300

    refactor: full photo popup component created

--- src/components/Main.js
+++ src/components/Main.js
@@ -99,14 +99,6 @@ function Main(props) {
                 button='Да'
             />
 
-            <section className="popup popup_full-photo">
-                <div className="full-photo">
-                    <button type="button" className="popup__close-button popup__close-button-full-photo"
-                        aria-label="закрыть"></button>
-                    <img className="full-photo__image" src="" alt="Большое и красивое фото" />
-                    <p className="full-photo__place"></p>
-                </div>
-            </section>
         </>
     )
 }
