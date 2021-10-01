bf1180128fdd575d0077b144dceed0fbeb149065
Marina Abovyan <marrina.abovyan@gmail.com>
2021-10-01 16:22:44 +0300

    refactor: extra tags removed

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,17 +1,15 @@
 function ImagePopup({ card, onClose }) {
     return (
-        <>
-            <section className={`popup popup popup_full-photo ${card && 'popup_opened'}`}>
-                <div className="full-photo">
-                    <button type="button" className="popup__close-button popup__close-button-full-photo"
-                        aria-label="закрыть"
-                        onClick={onClose}
-                    ></button>
-                    <img className="full-photo__image" src={card ? card.link : ''} alt={card ? card.name : ''} />
-                    <p className="full-photo__place">{card ? card.name : ''}</p>
-                </div>
-            </section>
-        </>
+        <section className={`popup popup popup_full-photo ${card && 'popup_opened'}`}>
+            <div className="full-photo">
+                <button type="button" className="popup__close-button popup__close-button-full-photo"
+                    aria-label="закрыть"
+                    onClick={onClose}
+                ></button>
+                <img className="full-photo__image" src={card ? card.link : ''} alt={card ? card.name : ''} />
+                <p className="full-photo__place">{card ? card.name : ''}</p>
+            </div>
+        </section>
     )
 }
 export default ImagePopup;
