774975d617ac0c783eb30f0a1dca3db7031e0ce8
Marina Abovyan <marrina.abovyan@gmail.com>
2021-10-15 13:47:00 +0300

    feat: delete button is shown only if user is the owner of the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,20 @@
+import React, { useContext } from "react";
+import { CurrectUserContext } from "../contexts/CurrentUserContext";
 
 function Card({ onCardClick, card }) {
+
+    const currentUser = useContext(CurrectUserContext);
+
+    const isOwn = currentUser._id === card.owner._id
+
+    const cardDeleteButtonClassName = (`button ${isOwn ? 'element__delete-button_visible' : 'element__delete-button_hidden'}`)
 
     const handleCardClick = () => {
         onCardClick(card)
     }
     return (
         <article className="element">
-            <button type="button" className="button element__delete-button"></button>
+            <button type="button" className={cardDeleteButtonClassName}></button>
             <img className="element__photo" src={card.link} alt={card.name} onClick={handleCardClick} />
             <div className="element__name-block">
                 <h2 className="element__name">{card.name}</h2>
