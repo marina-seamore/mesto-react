8493afbf770694b5e9c24321d60da3ab7a8a14c6
Marina Abovyan <marrina.abovyan@gmail.com>
2021-10-26 14:04:11 +0300

    popups close by Esc and clicking outside popup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,6 +1,30 @@
+import React from "react";
+
 function ImagePopup({ card, onClose }) {
+
+    const isOpen = !!card;
+
+    React.useEffect(() => {
+        if (!isOpen) return;
+        const handleEsc = (e) => {
+            if (e.key === "Escape") {
+                onClose()
+            }
+        }
+        console.log(!card)
+        document.addEventListener('keydown', handleEsc);
+        return () => {
+            document.removeEventListener('keydown', handleEsc)
+        }
+    }, [isOpen, onClose])
+
+    const handleOverlayClose = (e) => {
+        if (e.target === e.currentTarget && isOpen) {
+            onClose();
+        }
+    };
     return (
-        <section className={`popup popup popup_full-photo ${card && 'popup_opened'}`}>
+        <section className={`popup popup popup_full-photo ${card && 'popup_opened'}`} onMouseDown={handleOverlayClose}>
             <div className="full-photo">
                 <button type="button" className="popup__close-button popup__close-button-full-photo"
                     aria-label="закрыть"
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,14 +1,36 @@
-function PopupWithForm(props) {
+import React from "react"
 
-    const popupClassName = (`popup popup_${props.name} ${props.isOpen ? 'popup_opened' : ''}`)
+function PopupWithForm({name, isOpen, onSubmit, title, children, button, onClose}) {
+
+    const popupClassName = (`popup popup_${name} ${isOpen ? 'popup_opened' : ''}`)
+
+    React.useEffect(() => {
+        if (!isOpen) return;
+        const handleEsc = (e) => {
+            if (e.key === "Escape") {
+                onClose()
+            }
+        }
+        console.log('useEf')
+        document.addEventListener('keydown', handleEsc);
+        return () => {
+            document.removeEventListener('keydown', handleEsc)
+        }
+    }, [isOpen, onClose])
+
+    const handleOverlayClose = (e) => {
+        if (e.target === e.currentTarget && isOpen) {
+          onClose();
+        }
+      };
 
     return (
-        <section className={popupClassName}>
-            <form className='popup__content' name={props.name} onSubmit={props.onSubmit}>
-                <h3 className='popup__header'>{props.title}</h3>
-                {props.children}
-                <button type="submit" className="popup__submit-button">{props.button}</button>
-                <button type="button" className="popup__close-button" aria-label="закрыть" onClick={props.onClose}></button>
+        <section className={popupClassName} onMouseDown={handleOverlayClose}>
+            <form className='popup__content' name={name} onSubmit={onSubmit}>
+                <h3 className='popup__header'>{title}</h3>
+                {children}
+                <button type="submit" className="popup__submit-button">{button}</button>
+                <button type="button" className="popup__close-button" aria-label="закрыть" onClick={onClose}></button>
             </form>
         </section>
     )
